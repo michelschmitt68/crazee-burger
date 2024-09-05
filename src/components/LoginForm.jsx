5556361cc700a30d4359194033723eb7ea6cf5af
asmfc <michel081192@hotmail.fr>
2024-09-05 20:31:37 +0200

    ✏️ F01: LOGIN > Fix typo

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
     return(
 
         <form action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous</h1>
+            <h1>Bienvenue chez nous !</h1>
             <br />
             <h2>Connectez-vous</h2>
             <input
