50a91759a3a7fee1b5df5873c657f319dab81bfc
asmfc <michel081192@hotmail.fr>
2024-09-09 10:40:58 +0200

    ✨  F03: STYLE < Add css to loginPage

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
+import {theme} from '../../theme/index';
+import { HiUserCircle } from "react-icons/hi";
 
 export default function LoginForm() {
 
@@ -20,18 +22,22 @@ export default function LoginForm() {
     return(
 
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-                <h1>Bienvenue chez nous !</h1>
-                <br />
-                <h2>Connectez-vous</h2>
-            <input
-                type="text"
-                placeholder="Entrez votre prénom"
-                required 
-                value={inputValue} 
-                onChange={handleChange} 
-            />
+            <div className="title">Bienvenue chez nous !</div>
+            <div className="orange-bar" />
+            <div className="connect-label">Connectez-vous</div>
+            
+            <div className="input-container">
+                <HiUserCircle className="icon" /> {/* Icône ajoutée ici */}
+                <input
+                    type="text"
+                    placeholder="Entrez votre prénom"
+                    required
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+            </div>
             <button >
-                Accéder à votre espace
+                Accéder à mon espace   &gt;
             </button>
         </LoginFormStyled>
 
@@ -43,5 +49,73 @@ const LoginFormStyled = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: cadetblue;
+    color: ${theme.colors.white};
+    font-family: 'Amatic SC', cursive;
+    font-weight: ${theme.weights.bold};
+
+    .title{
+        font-size: ${theme.fonts.P5};
+        margin: 39px 45px 32px 45px;
+    }
+
+    .orange-bar{
+        width: 100%;
+        background-color: #F56A2C;
+        height: 3px;
+    }
+    .connect-label{
+        font-size: ${theme.fonts.P4};
+        margin-top: 40px;
+    }
+
+    .input-container {
+        position: relative;
+        width: 400px;
+
+
+        .icon {
+            position: absolute;
+            left: 24px;
+            top: 37px;
+            color: ${theme.colors.greyDark};
+            font-size: ${theme.fonts.P2};
+
+        }
+
+        input {
+            width: 400px;
+            height: 55px;
+            padding-left: 60px;
+            padding-right: 24px;
+            margin-top: 18px;
+            margin-bottom: 18px;
+            border-radius: ${theme.borderRadius.round};
+            border: none;
+            box-sizing: border-box;
+            font-size: ${theme.fonts.P0};
+            letter-spacing: 0.5px;
+            &::placeholder {
+                color: ${theme.colors.greyMedium}; 
+                font-weight: ${theme.weights.light};
+                font-size: ${theme.fonts.P0};
+                font-family: Arial;
+            }
+        }
+    }
+
+
+    button{
+        width: 400px;
+        height: 53px;
+        background-color: ${theme.colors.primary};
+        color: ${theme.colors.white};
+        border: none;
+        border-radius: ${theme.borderRadius.round};
+        font-weight: ${theme.weights.bold};
+        font-family: Arial;
+        font-size: ${theme.fonts.P0};
+        padding: 18px 24px ;
+    }
+    
+
 `;
