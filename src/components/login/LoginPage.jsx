eb3a988243d9890fb618b461fb13a1d8fd29ee98
asmfc <michel081192@hotmail.fr>
2024-09-10 16:32:54 +0200

    💄 F03: LOGIN PAGE < Add filter background

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -17,11 +17,23 @@ export default function Login() {
 const LoginPageStyled = styled.div`
   
     height: 100vh;
-    width: 100%;
-    background-image: url(${backgroundImage});
-    background-size: cover;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
+
+
+    &::before {
+    content: '';
+    background: url(${backgroundImage}) rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
   `
