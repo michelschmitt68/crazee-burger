3580b135fc3f6bcc96de05dd4bcc5ad0491f6cb2
asmfc <michel081192@hotmail.fr>
2024-09-10 15:31:13 +0200

    📦   F03 : LoginPage < Add package props-types

--- src/components/login/InputText.jsx
+++ src/components/login/InputText.jsx
@@ -1,22 +1,25 @@
-
+import PropTypes from 'prop-types';
 import { BsPersonCircle } from "react-icons/bs";
 
-
 const InputText = ({inputValue, onChange, placeholder}) => {
-    
   return (
-    
     <div className="input-container">
-                <BsPersonCircle className="icon" /> {/* Icône ajoutée ici */}
-                <input
-                    type="text"
-                    required
-                    value={inputValue}
-                    onChange={onChange}
-                    placeholder={placeholder}
-                />
+      <BsPersonCircle className="icon" />
+      <input
+        type="text"
+        required
+        value={inputValue}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
   )
 }
 
-export default InputText
+InputText.propTypes = {
+  inputValue: PropTypes.string.isRequired, 
+  onChange: PropTypes.func.isRequired,     
+  placeholder: PropTypes.string            
+};
+
+export default InputText;
