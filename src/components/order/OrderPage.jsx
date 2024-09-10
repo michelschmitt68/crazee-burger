ab0a48e9d5b85ae0575964179f94548e0a81d0b3
asmfc <michel081192@hotmail.fr>
2024-09-10 23:01:23 +0200

    🎨 F04: ORDERPAGE< Add container sizing

--- src/components/order/OrderPage.jsx
+++ src/components/order/OrderPage.jsx
@@ -1,22 +1,47 @@
-import { useNavigate, useParams } from "react-router-dom";
+// import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "./Navbar";
 import Main from "./Main";
+import styled from "styled-components";
+import { theme } from "../../theme";
 
 const OrderPage = () => {
 
-    const { username } = useParams();
-    const navigate = useNavigate();
+    // const { username } = useParams();
+    // const navigate = useNavigate();
 
   return (
-    <div className="container">
-      <h1>Bonjour {username}</h1>
+    <OrderPageStyled>
+      {/* <h1>Bonjour {username}</h1>
       <button   
-        onClick={() => navigate(`/`)}>Déconnexion</button>
+        onClick={() => navigate(`/`)}>Déconnexion</button> */}
     
       <Navbar></Navbar>
       <Main></Main>
-    </div>
+    </OrderPageStyled>
   )
 }
 
 export default OrderPage
+
+
+const OrderPageStyled = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 25px 56px;
+    background-color: green;
+    max-width: 1400px;
+    min-width: 1400px;
+    
+
+    &::before{
+      content: '';
+      background-color: ${theme.colors.primary};
+      position: absolute; 
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh; 
+      z-index: -1; 
+    }
+`;
