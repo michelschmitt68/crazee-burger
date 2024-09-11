31c3a1b687ba94f43fd8eae51b7568acce4ca328
asmfc <michel081192@hotmail.fr>
2024-09-11 17:47:21 +0200

    🚧 F05: OrderPage < Create Layout grid burgers

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -4,16 +4,39 @@ import { theme } from "../../../theme";
 export default function Main() {
   return (
     <MainStyled>
-
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 2</div>
+      <div className="grid-item">Item 3</div>
+      <div className="grid-item">Item 4</div>
+      <div className="grid-item">Item 5</div>
+      <div className="grid-item">Item 6</div>
+      <div className="grid-item">Item 7</div>
+      <div className="grid-item">Item 8</div>
+      <div className="grid-item">Item 9</div>
+      <div className="grid-item">Item 10</div>
     </MainStyled>
   )
 }
 
 const MainStyled = styled.div`  
-    background-color: ${theme.colors.  background_white};
-    height: 100%;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    justify-items: center;
+    align-items: center;
+    padding: 50px 92px;
+
+    background-color: ${theme.colors.background_white};
+    height: 100vh;  /* Assure que la hauteur prend tout l'écran */
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  
+
+    .grid-item {
+      background-color: #a7a7df;
+      width: 240px;
+      height: 330px;
+      display: flex;
+      justify-content: center;  /* Centre le contenu de chaque grid-item horizontalement */
+      align-items: center;      /* Centre le contenu de chaque grid-item verticalement */
+    }
 `;
